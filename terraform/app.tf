ff0718c13ceb55c36a8d9df55fcd843d52b67cb7
sivasfdc <ssaminathan@salesforce.com>
2021-04-19 12:44:50 +0530

    Update app.tf

--- terraform/app.tf
+++ terraform/app.tf
@@ -26,7 +26,7 @@ terraform {
    backend "s3" {
  # Replace this with your bucket name!
    bucket         = "ot-infra-state"
-   key            = "global/s3/terraform.tfstate"
+   key            = "global/s3/terraform-holly-drill.tfstate"
    region         = "us-east-1"
    }
 }
